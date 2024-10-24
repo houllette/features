3033a4f12c0c54dd4bfc14436efbb4e069fc0f47
Holden Oullette <6202965+houllette@users.noreply.github.com>
2024-10-24 20:53:20 +0000

    Initial skeleton

---
+++ src/elixir/README.md
@@ -0,0 +1,21 @@
+
+# Installs Elixir (elixir)
+
+A feature to install and use the Elixir ecosystem
+
+## Example Usage
+
+```json
+"features": {
+    "ghcr.io/houllette/features/elixir:1": {
+        "version": "latest"
+    }
+}
+```
+
+## Options
+
+| Options Id | Description | Type | Default Value |
+|-----|-----|-----|-----|
+| version | Choose your installed Elixir version. | string | latest |
+
---
+++ src/elixir/install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+echo "Activating feature 'elixir'"
+
+# The 'install.sh' entrypoint script is always executed as the root user.
+#
+# These following environment variables are passed in by the dev container CLI.
+# These may be useful in instances where the context of the final 
+# remoteUser or containerUser is useful.
+# For more details, see https://containers.dev/implementors/features#user-env-var
+echo "The effective dev container remoteUser is '$_REMOTE_USER'"
+echo "The effective dev container remoteUser's home directory is '$_REMOTE_USER_HOME'"
+
+echo "The effective dev container containerUser is '$_CONTAINER_USER'"
+echo "The effective dev container containerUser's home directory is '$_CONTAINER_USER_HOME'"
+
+ELIXIR_VERSION="${VERSION:-"latest"}" # Default to latest version
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
